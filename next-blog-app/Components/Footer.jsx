9a9dca07387ec278eb75749d3e20c97c0dc3c59e
Mari Hirota <mari.hirota97@gmail.com>
2024-07-02 22:56:18 -0500

    Created a list of blogs and footer of the page

---
+++ next-blog-app/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import { assets } from '@/Assets/assets'
+import React from 'react'
+import Image from 'next/image'
+
+const Footer = () =>{
+    return(
+        <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 item-center'>
+            <Image src={assets.logo_light} alt='' width={120}/>
+            <p className='text-sm text-white'>All right reserved. Copyright @lallalaDiary</p>
+            <div className='flex'>
+                <Image src={assets.facebook_icon} alt='' width={40}/>
+                <Image src={assets.twitter_icon} alt='' width={40}/>
+                <Image src={assets.googleplus_icon} alt='' width={40}/>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- next-blog-app/app/page.js
+++ next-blog-app/app/page.js
@@ -1,12 +1,14 @@
 'use client'
-import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
+import Footer from "@/Components/Footer";
 import Header from "@/Components/Header"
 
 export default function Home() {
   return (
     <>
     <Header/>
-    <BlogItem/>
+    <BlogList/>
+    <Footer/>
     </>
   );
 }
