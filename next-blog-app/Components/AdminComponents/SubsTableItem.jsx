a84bb544076ac891079fd8243c4d8d542e48fed1
Mari Hirota <mari.hirota97@gmail.com>
2024-07-09 23:50:01 -0500

    WIP: subscription page

---
+++ next-blog-app/Components/AdminComponents/SubsTableItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SubsTableItem = ({email}) => {
+  return (
+    <tr className='bg-white border-b text-left'>
+        <th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+            {email?email:"No Email"}
+        </th>
+        <td className='px-6 py-4 hidden sm:block'>
+            {"11 Jan 2024"}
+        </td>
+        <td className='px-6 py-4 cursor-pointer'>
+            x
+        </td>
+    </tr>
+  )
+}
+
+export default SubsTableItem
--- next-blog-app/app/admin/subscriptions/page.jsx
+++ next-blog-app/app/admin/subscriptions/page.jsx
@@ -1,9 +1,45 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SubsTableItem from '@/Components/AdminComponents/SubsTableItem'
+import axios from 'axios';
 
 const page = () => {
+
+  const [emails, setEmails] = useState([]);
+
+  const fetchEmails = async() =>{
+    const response = await axios.get('/api/email');
+    setEmails(response.data.emails);
+  }
+
+  useEffect (() => {
+    fetchEmails();
+  }, [])
+
   return (
-    <div>page</div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <h1>All Subscription</h1>
+      <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
+        <table className='w-full text-sm text-gray-500'>
+          <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
+            <th scope='col' className='px-5 py-3'>
+              Email Subscription
+            </th>
+            <th scope='col' className='hidden sm:block px-5 py-3'>
+              Data
+            </th>
+            <th scope='col' className='px-5 py-3'>
+              Action
+            </th>
+          </thead>
+          <tbody>
+            {emails.map((item, index)=>{
+              return <SubsTableItem key={index} mongoId={item._id} email={item.email}/>
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
