762b00b5fc77d22a21ed707b43bdd3ab1a432949
Mari Hirota <mari.hirota97@gmail.com>
2024-07-14 23:28:38 -0500

    added email delete function

--- next-blog-app/Components/AdminComponents/BlogTableItem.jsx
+++ next-blog-app/Components/AdminComponents/BlogTableItem.jsx
@@ -6,10 +6,10 @@ const BlogTableItem = ({authorImg, title, author, date, deleteBlog, mongoId}) =>
     const BlogDate = new Date(date);
   return (
     <tr className='bg-white border-b'>
-        <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+        <td scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
             <Image width={40} height={40} src={authorImg?authorImg:assets.profile_icon}/>
             <p>{author?author:"No author"}</p>
-        </th>
+        </td>
         <td className='px-6 py-4'>
             {title?title:"No title"}
         </td>
--- next-blog-app/app/admin/subscriptions/page.jsx
+++ next-blog-app/app/admin/subscriptions/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import SubsTableItem from '@/Components/AdminComponents/SubsTableItem'
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const page = () => {
 
@@ -10,6 +11,22 @@ const page = () => {
   const fetchEmails = async() =>{
     const response = await axios.get('/api/email');
     setEmails(response.data.emails);
+  }
+
+  const deleteEmail = async (mongoId) =>{
+    const response = await axios.delete('/api/email', {
+      params:{
+        id: mongoId
+      }
+    });
+    if(response.data.success){
+      toast.success(response.data.msg);
+      fetchEmails();
+    }
+    else{
+      toast.error("Error");
+    }
+    
   }
 
   useEffect (() => {
@@ -34,7 +51,7 @@ const page = () => {
           </thead>
           <tbody>
             {emails.map((item, index)=>{
-              return <SubsTableItem key={index} mongoId={item._id} email={item.email}/>
+              return <SubsTableItem key={index} mongoId={item._id} date={item.date} email={item.email} deleteEmail={deleteEmail}/>
             })}
           </tbody>
         </table>
